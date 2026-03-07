198d174ef8f531b79f102dc210ae0376fbc6aa09
Md-Easin-Ali-Khane <mdeasinalikhan9@gmail.com>
2026-03-07 14:56:39 +0600

    The issues tracker section is done without functionality

--- script.js
+++ script.js
@@ -2,16 +2,16 @@ const loginPage = document.getElementById("login-page");
 const loginBtn = document.getElementById("login-btn");
 const mainPage = document.getElementById("main-page")
 
-// loginBtn.addEventListener("click", () => {
-//     const userName = document.getElementById("user-name");
-//     const password = document.getElementById("password");
+loginBtn.addEventListener("click", () => {
+    const userName = document.getElementById("user-name");
+    const password = document.getElementById("password");
 
-//     if (userName.value === "admin" && password.value === "admin123") {
-//         loginPage.classList.add("hidden")
-//         mainPage.classList.remove("hidden")
+    if (userName.value === "admin" && password.value === "admin123") {
+        loginPage.classList.add("hidden")
+        mainPage.classList.remove("hidden")
 
-//     } else {
-//         alert("Invalid user")
-//     }
+    } else {
+        alert("Invalid user")
+    }
 
-// })
+})
